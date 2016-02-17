33de55123aa89f5b56798a62e1478797294943c1
Nathan Kot <me@nathankot.com>
2016-02-17 17:22:47 +1300

    Persist state in localStorage

--- web/src/store.js
+++ web/src/store.js
@@ -1,13 +1,38 @@
 import Rx from 'rx'
+import moment from 'moment-timezone'
+
+// One of two evils:
+// store auth in cookie = CSRF
+// store auth in localStorage = XSS
+const localStorage = window.localStorage
+
+var initialUser =
+    (localStorage && localStorage.getItem('user')) ||
+    null
+
+var initialTimezone =
+    (localStorage && localStorage.getItem('timezone')) ||
+    moment.tz.guess()
 
 const store = {
   isLoggedIn: false,
-  currentUser: new Rx.BehaviorSubject(null)
+  currentUser: new Rx.BehaviorSubject(initialUser),
+  timezone: new Rx.BehaviorSubject(initialTimezone)
 }
 
 store.currentUser
   .subscribeOnNext(u => {
     store.isLoggedIn = !!u
+    if (localStorage) {
+      localStorage.setItem('user', u)
+    }
+  })
+
+store.timezone
+  .subscribeOnNext(z => {
+    if (localStorage) {
+      localStorage.setItem('timezone', z)
+    }
   })
 
 export default store
