0c31da900ecc03419e960c82493bfa1a7fcf2200
Nathan Kot <me@nathankot.com>
2016-02-23 00:36:20 +1300

    Style error messages

--- web/src/components/errorMessage.vue
+++ web/src/components/errorMessage.vue
@@ -3,3 +3,19 @@
     <slot></slot>
   </div>
 </template>
+
+<style lang="sass">
+ @import 'src/settings';
+ @import 'src/mixins';
+
+ .message {
+   padding: em(16);
+   border-radius: em(6);
+   margin-bottom: em(12);
+ }
+
+ .message-error {
+   color: red;
+   background-color: tint(red, 92%);
+ }
+</style>
